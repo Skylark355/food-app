7c97b30aed84aa7a749af7c6bf7976cb105262d8
Skylark355 <adzesi.chris@gmail.com>
2025-02-24 13:53:26 +0000

    Almost done with the home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,17 @@
 import Navbar from "../../Components/Navbar/Navbar";
-import Header from "../../Components/Header/Header"
+import Header from "../../Components/Header/Header";
 import ExploreMenu from "../../Components/ExploreMenu/ExploreMenu";
+import FoodDisplay from "../../Components/FoodDisplay/FoodDisplay";
+import { useState } from "react";
 
 function Home() {
+  const [category, setCategory] = useState("All");
   return (
     <>
       <Navbar />
       <Header />
-      <ExploreMenu />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+// import { StoreContextProvider } from "./context/";
+import StoreContextProvider from "./context/StoreContexts.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
     </BrowserRouter>
   </StrictMode>
 );
